216b4a8ffb725d3fff93f3696c39791692cc7a88
tfboys72899 <728991524@qq.com>
2022-01-06 10:42:17 +0800

    visualize

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 # @Time: 2022/fbx/2 上午10:18
 
-from utils import *
+from utils.visualize_frames import visualise_frames
 import numpy as np
 
 arr = np.load('data/npz/JDM_motion.npz')
 print(arr['data'].shape)
 print(arr['label'])
+
+visualise_frames()
--- utils/visualize_frames.py
+++ utils/visualize_frames.py
@@ -4,7 +4,7 @@ import os
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
-def visualise_frames(pathtonpy='json2npy/Aim/Aim.npy',up_view=0,side_view=90,x_lim=[-2,2],y_lim=[-2,2],z_lim=[-2,2]):
+def visualise_frames(pathtonpy='data/npz/JDM_motion.npz',up_view=0,side_view=90,x_lim=[-2,2],y_lim=[-2,2],z_lim=[-2,2]):
     #MIXAMO DATASET
     """
     Each POSE follow the given index joints below:
@@ -26,31 +26,46 @@ def visualise_frames(pathtonpy='json2npy/Aim/Aim.npy',up_view=0,side_view=90,x_l
         14 - Rankle
     """
     #Load .npy file
-    motion3d = np.load(pathtonpy)
+    motion3d = np.load(pathtonpy)['data']
+    motion3d = motion3d.reshape(1,77,22,3)
+    motion3d = motion3d[0]
+    motion3d = motion3d.transpose(1,2,0)
 
     #Index for bone links
-    bones = [[0,1],[1,2],[2,3],[3,4],[1,5],[5,6],[6,7],[1,8],[8,9],[9,10],[10,11],[8,12],[12,13],[13,14]]
+    bones = [[0,1],[1,2],[1,3],[2,4],[2,14],[4,6],[6,12],[6,10],[6,8],[3,5],[3,15],[5,7],[7,13],[7,11],[7,9],[14,15],[14,16],[16,18],[18,20],[15,17],[17,19],[19,21]]
 
     #Set offset for few frame visualisation
     offset = int(motion3d.shape[2]/5)
     offset_sum = 0
+    fig = plt.figure()
+    plt.ion()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.view_init(elev=140,azim=-90)
 
     #Output frames
-    for i in range(5):
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-        ax.scatter(motion3d[:,0,offset_sum],motion3d[:,1,offset_sum],motion3d[:,2,offset_sum])
+    for i in range(motion3d.shape[2]):
+        ax.lines.clear()
+        # ax.scatter(motion3d[:,0,i],motion3d[:,1,i],motion3d[:,2,i])
         ax.set_xlim(x_lim)
         ax.set_ylim(y_lim)
         ax.set_zlim(z_lim)
         for i in range(len(bones)):
-            ax.plot3D([motion3d[bones[i][0],0,offset_sum],motion3d[bones[i][1],0,offset_sum]],[motion3d[bones[i][0],1,offset_sum],motion3d[bones[i][1],1,offset_sum]],[motion3d[bones[i][0],2,offset_sum],motion3d[bones[i][1],2,offset_sum]])
+            ax.plot3D([motion3d[bones[i][0],0,offset_sum],motion3d[bones[i][1],0,offset_sum]],[motion3d[bones[i][0],1,offset_sum],motion3d[bones[i][1],1,offset_sum]],[motion3d[bones[i][0],2,offset_sum],motion3d[bones[i][1],2,offset_sum]], c="#3498db")
 
-        ax.view_init(up_view,side_view)
-        print(offset_sum)
-        ax.set_title('Frame: %d'%(offset_sum))
-        offset_sum = offset_sum+offset
+        RADIUS = 0.5  # space around the subject
+        xroot, yroot, zroot = motion3d[1, 0, i], motion3d[1, 1, i], motion3d[1, 2, i]
+        ax.set_xlim3d([-RADIUS + xroot, RADIUS + xroot])
+        ax.set_zlim3d([-RADIUS, 2 * RADIUS + zroot])
+        ax.set_ylim3d([-RADIUS + yroot, RADIUS + yroot])
 
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("z")
+
+        plt.pause(0.001)
+        # ax.set_title('Frame: %d'%(offset_sum))
+
+    plt.ioff()
     plt.show()
 
 if __name__ == '__main__':
